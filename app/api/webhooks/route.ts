c8e34979d7d8edbf71b9b1c3a342354f2003c801
Mohamad Masalmeh <mohamad.msalme77@gmail.com>
2024-02-03 23:41:18 +0300

    test function

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -4,6 +4,7 @@ import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
@@ -52,7 +53,8 @@ export async function POST(req: Request) {
   }
 
   const eventType = evt.type;
-  console.log(eventType);
+  const test = auth();
+  console.log(test);
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
